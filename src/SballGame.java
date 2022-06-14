e2383778dd94a448758fd734a29b956a7868b827
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-06-14 16:20:57 -0400

    Create SballGame.java

---
+++ src/SballGame.java
@@ -0,0 +1,13 @@
+import game.GameFrame;
+import menu.HomeFrame;
+
+public class SballGame {
+
+    public static void main(String[] args) {
+
+        //HomeFrame homeFrame = new menu.HomeFrame(); // Menu Window Frame
+        GameFrame gameFrame = new game.GameFrame(); // Game Frame
+    }
+}
+
+// ToDo - Make a Home screen
